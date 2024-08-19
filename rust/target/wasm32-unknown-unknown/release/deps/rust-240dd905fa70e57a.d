00539455b8a762d89309da06767bd943ab022b73
ArthurLobopro <ArthurLobopro@users.noreply.github.com>
2024-08-19 22:52:45 +0000

    deploy: a9895c7251c1069b20e0241ea6296ab10677aa6a

---
+++ rust/target/wasm32-unknown-unknown/release/deps/rust-240dd905fa70e57a.d
@@ -0,0 +1,5 @@
+/home/runner/work/wasm-fizzbuzz/wasm-fizzbuzz/rust/target/wasm32-unknown-unknown/release/deps/rust-240dd905fa70e57a.wasm: src/main.rs
+
+/home/runner/work/wasm-fizzbuzz/wasm-fizzbuzz/rust/target/wasm32-unknown-unknown/release/deps/rust-240dd905fa70e57a.d: src/main.rs
+
+src/main.rs:
